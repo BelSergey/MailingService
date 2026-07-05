b6bde3e7016e6d5a168b3602489ffd3b2cde2ec5
Sergey Belyakov <belserg2002@gmail.com>
2026-07-05 17:36:39 +0300

    подготовка settings.py под аутентификацию, шаблоны и кэш

--- config/settings.py
+++ config/settings.py
@@ -117,20 +117,20 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-# AUTHENTICATION_BACKENDS = [
-#     'users.backends.EmailBackend',
-# ]
-#
-# AUTH_USER_MODEL = 'users.User'
-#
-# LOGIN_URL = '/users/login/'
+AUTHENTICATION_BACKENDS = [
+       "django.contrib.auth.backends.ModelBackend",
+   ]
+AUTH_USER_MODEL = "users.User"
+LOGIN_URL = "users:login"
+LOGIN_REDIRECT_URL = "mailing:home"
+LOGOUT_REDIRECT_URL = "users:login"
 
 
 CACHES = {
@@ -139,3 +139,4 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
+CACHE_ENABLED = os.getenv("cache_enabled", "True") == "True"
